0e02e067baa0f32eef7fd151e89f79af1a74ccc2
mnz0y <minji6035@naver.com>
2024-03-15 17:32:29 +0900

    20240315

--- js/index_ajax.js
+++ js/index_ajax.js
@@ -1,210 +1,270 @@
 // 오늘의선택-전체
 $.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=contents",
+  data: { query: "21세기 북스", size: 50 },
+  headers: { Authorization: "KakaoAK 8244a385855c795ceaadd4a47e53aa38" }
+})
+  .done(function (msg) {
+    //오늘의 책 왼쪽
+    const today = $('.Lbook0');
+
+    for (let i = 0; i < today.length; i++) {
+      $('.Lbook0').eq(i).prepend('<a href="#"><img src="' + msg.documents[i].thumbnail + '"/></a>');
+      $('.book0').eq(i).append('<h2>' + msg.documents[i].title + '</h2>');
+      $('.book0').eq(i).append('<p>' + msg.documents[i].authors + '</p>');
+
+      let priceDot = msg.documents[i].sale_price;
+      let dot = priceDot.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+      $('.book0').eq(i).append('<span>10%</span>');
+      $('.book0').eq(i).append('<span>' + dot + '원</span>');
+
+      let str = msg.documents[i].contents;
+      let str2 = str.substring(0, 100);
+      $('.book0').eq(i).append('<p>' + str2 + '</p>');
+    }
+
+    //오늘의 책 오른쪽
+    const todaySlide = $('.slidebook0');
+    for (let i = 0; i < todaySlide.length; i++) {
+      $('.slidebook0').eq(i).append('<a href="#"><img src="' + msg.documents[i].thumbnail + '"/></a>');
+      $('.slidebook0').eq(i).append('<p>' + msg.documents[i].title + '</p>');
+    }
+  });
+
+//국내도서
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=contents",
+  data: { query: "소설", size: 50 },
+  headers: { Authorization: "KakaoAK 8244a385855c795ceaadd4a47e53aa38" }
+})
+  .done(function (msg) {
+    //오늘의 책 왼쪽
+    const today = $('.Lbook1');
+
+    for (let i = 0; i < today.length; i++) {
+      $('.Lbook1').eq(i).prepend('<a href="#"><img src="' + msg.documents[i].thumbnail + '"/></a>');
+      $('.book1').eq(i).append('<h2>' + msg.documents[i].title + '</h2>');
+      $('.book1').eq(i).append('<p>' + msg.documents[i].authors + '</p>');
+
+      let priceDot = msg.documents[i].sale_price;
+      let dot = priceDot.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+      $('.book1').eq(i).append('<span>10%</span>');
+      $('.book1').eq(i).append('<span>' + dot + '원</span>');
+
+      let str = msg.documents[i].contents;
+      let str2 = str.substring(0, 100);
+      $('.book1').eq(i).append('<p>' + str2 + '</p>');
+    }
+
+    //오늘의 책 오른쪽
+    const todaySlide = $('.slidebook1');
+    for (let i = 0; i < todaySlide.length; i++) {
+      $('.slidebook1').eq(i).append('<a href="#"><img src="' + msg.documents[i].thumbnail + '"/></a>');
+      $('.slidebook1').eq(i).append('<p>' + msg.documents[i].title + '</p>');
+    }
+  });
+
+//eBook
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=contents",
+  data: { query: "에세이", size: 50 },
+  headers: { Authorization: "KakaoAK 8244a385855c795ceaadd4a47e53aa38" }
+})
+  .done(function (msg) {
+    //오늘의 책 왼쪽
+    const today = $('.Lbook2');
+
+    for (let i = 0; i < today.length; i++) {
+      $('.Lbook2').eq(i).prepend('<a href="#"><img src="' + msg.documents[i].thumbnail + '"/></a>');
+      $('.book2').eq(i).append('<h2>' + msg.documents[i].title + '</h2>');
+      $('.book2').eq(i).append('<p>' + msg.documents[i].authors + '</p>');
+
+      let priceDot = msg.documents[i].sale_price;
+      let dot = priceDot.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+      $('.book2').eq(i).append('<span>10%</span>');
+      $('.book2').eq(i).append('<span>' + dot + '원</span>');
+
+      let str = msg.documents[i].contents;
+      let str2 = str.substring(0, 100);
+      $('.book2').eq(i).append('<p>' + str2 + '</p>');
+    }
+
+    //오늘의 책 오른쪽
+    const todaySlide = $('.slidebook2');
+    for (let i = 0; i < todaySlide.length; i++) {
+      $('.slidebook2').eq(i).append('<a href="#"><img src="' + msg.documents[i].thumbnail + '"/></a>');
+      $('.slidebook2').eq(i).append('<p>' + msg.documents[i].title + '</p>');
+    }
+  });
+
+//sam
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=publisher",
+  data: { query: "민음사", size: 50 },
+  headers: { Authorization: "KakaoAK 8244a385855c795ceaadd4a47e53aa38" }
+})
+  .done(function (msg) {
+    //오늘의 책 왼쪽
+    const today = $('.Lbook3');
+
+    for (let i = 0; i < today.length; i++) {
+      $('.Lbook3').eq(i).prepend('<a href="#"><img src="' + msg.documents[i].thumbnail + '"/></a>');
+      $('.book3').eq(i).append('<h2>' + msg.documents[i].title + '</h2>');
+      $('.book3').eq(i).append('<p>' + msg.documents[i].authors + '</p>');
+
+      let priceDot = msg.documents[i].sale_price;
+      let dot = priceDot.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+      $('.book3').eq(i).append('<span>10%</span>');
+      $('.book3').eq(i).append('<span>' + dot + '원</span>');
+
+      let str = msg.documents[i].contents;
+      let str2 = str.substring(0, 100);
+      $('.book3').eq(i).append('<p>' + str2 + '</p>');
+    }
+
+    //오늘의 책 오른쪽
+    const todaySlide = $('.slidebook3');
+    for (let i = 0; i < todaySlide.length; i++) {
+      $('.slidebook3').eq(i).append('<a href="#"><img src="' + msg.documents[i].thumbnail + '"/></a>');
+      $('.slidebook3').eq(i).append('<p>' + msg.documents[i].title + '</p>');
+    }
+  });
+
+//핫트랙스
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=publisher",
+  data: { query: "창비", size: 50 },
+  headers: { Authorization: "KakaoAK 8244a385855c795ceaadd4a47e53aa38" }
+})
+  .done(function (msg) {
+    //오늘의 책 왼쪽
+    const today = $('.Lbook4');
+
+    for (let i = 0; i < today.length; i++) {
+      $('.Lbook4').eq(i).prepend('<a href="#"><img src="' + msg.documents[i].thumbnail + '"/></a>');
+      $('.book4').eq(i).append('<h2>' + msg.documents[i].title + '</h2>');
+      $('.book4').eq(i).append('<p>' + msg.documents[i].authors + '</p>');
+
+      let priceDot = msg.documents[i].sale_price;
+      let dot = priceDot.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+      $('.book4').eq(i).append('<span>10%</span>');
+      $('.book4').eq(i).append('<span>' + dot + '원</span>');
+
+      let str = msg.documents[i].contents;
+      let str2 = str.substring(0, 100);
+      $('.book4').eq(i).append('<p>' + str2 + '</p>');
+    }
+
+    //오늘의 책 오른쪽
+    const todaySlide = $('.slidebook4');
+    for (let i = 0; i < todaySlide.length; i++) {
+      $('.slidebook4').eq(i).append('<a href="#"><img src="' + msg.documents[i].thumbnail + '"/></a>');
+      $('.slidebook4').eq(i).append('<p>' + msg.documents[i].title + '</p>');
+    }
+  });
+
+//교보ONly
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=contents",
+  data: { query: "교보문고", size: 50 },
+  headers: { Authorization: "KakaoAK 8244a385855c795ceaadd4a47e53aa38" }
+})
+  .done(function (msg) {
+    //오늘의 책 왼쪽
+    const today = $('.Lbook5');
+
+    for (let i = 0; i < today.length; i++) {
+      $('.Lbook5').eq(i).prepend('<a href="#"><img src="' + msg.documents[i].thumbnail + '"/></a>');
+      $('.book5').eq(i).append('<h2>' + msg.documents[i].title + '</h2>');
+      $('.book5').eq(i).append('<p>' + msg.documents[i].authors + '</p>');
+
+      let priceDot = msg.documents[i].sale_price;
+      let dot = priceDot.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      $('.book5').eq(i).append('<span>10%</span>');
+      $('.book5').eq(i).append('<span>' + dot + '원</span>');
+
+      let str = msg.documents[i].contents;
+      let str2 = str.substring(0, 100);
+      $('.book5').eq(i).append('<p>' + str2 + '</p>');
+    }
+
+    //오늘의 책 오른쪽
+    const todaySlide = $('.slidebook5');
+    for (let i = 0; i < todaySlide.length; i++) {
+      $('.slidebook5').eq(i).append('<a href="#"><img src="' + msg.documents[i].thumbnail + '"/></a>');
+      $('.slidebook5').eq(i).append('<p>' + msg.documents[i].title + '</p>');
+    }
+  });
+
+
+//컨텐츠4-비디오
+let video;
+video =["이반 일리치의 죽음", "한국축구, 바꿔야산다!", "홍보의 신", "요즘 어른을 위한 최소한의 문해력"]
+for(let i =0; i<video.length;i++){
+  $.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: video[i], size: 15 },
+    headers: { Authorization: "KakaoAK 8244a385855c795ceaadd4a47e53aa38" }
+  })
+    .done(function (msg) {
+      const origin = msg.documents;
+      let data = origin.filter((val) => {
+        return val.thumbnail !== "" && val.title !='';
+      });
+
+      $('.bookbox4').eq(i).append('<img src = "'+origin[0].thumbnail+'"/>');
+      $('.bookbox4').eq(i).append('<span>'+origin[0].title+'</span>');
+      
+    });
+}
+
+//컨텐츠7-picks
+let picks;
+picks =["wonka", "데일 카네기 인간관계론", "퍼스널 MBA", "빨간내복야코 맞춤법"]
+for(let i =0; i<picks.length;i++){
+  $.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: picks[i], size: 15 },
+    headers: { Authorization: "KakaoAK 8244a385855c795ceaadd4a47e53aa38" }
+  })
+    .done(function (msg) {
+      const origin = msg.documents;
+      let data = origin.filter((val) => {
+        return val.thumbnail !== "" && val.title !='';
+      });
+       $('.pickList').eq(i).append('<img src = "'+origin[0].thumbnail+'"/>');
+       $('.pickList').eq(i).append('<p>'+origin[0].title+'</p>');
+       $('.pickList').eq(i).append('<span>'+origin[0].authors+'</span>');     
+    });
+}
+
+
+// 컨텐츠8-히트
+  $.ajax({
     method: "GET",
     url: "https://dapi.kakao.com/v3/search/book?target=contents",
-    data: {query: "21세기 북스", size:50},
-    headers: {Authorization: "KakaoAK 8244a385855c795ceaadd4a47e53aa38"}
+    data: { query: '문학동네', size: 15 },
+    headers: { Authorization: "KakaoAK 8244a385855c795ceaadd4a47e53aa38" }
   })
-    .done(function( msg ) {
-      console.log(msg);
-      //오늘의 책 왼쪽
-      const today =$('.Lbook0');
-
-      for(let i =0; i<today.length; i++){
-        $('.Lbook0').eq(i).prepend('<a href="#"><img src="'+msg.documents[i].thumbnail+'"/></a>');
-        $('.book0').eq(i).append('<h2>'+msg.documents[i].title+'</h2>');
-        $('.book0').eq(i).append('<p>'+msg.documents[i].authors+'</p>');
-        
-        let priceDot = msg.documents[i].sale_price;
-        let dot = priceDot.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-        $('.book0').eq(i).append('<span>10%</span>');
-        $('.book0').eq(i).append('<span>'+dot+'원</span>');
-        
-        let str = msg.documents[i].contents;
-        let str2 = str.substring(0,100);
-        $('.book0').eq(i).append('<p>'+str2+'</p>');
-      }
-
-      //오늘의 책 오른쪽
-      const todaySlide=$('.slidebook0');
-      for(let i =0; i<todaySlide.length; i++){
-        $('.slidebook0').eq(i).append('<a href="#"><img src="'+msg.documents[i].thumbnail+'"/></a>');
-        $('.slidebook0').eq(i).append('<p>'+msg.documents[i].title+'</p>');
+    .done(function (msg) {
+      const origin = msg.documents;
+      let data = origin.filter((val) => {
+      return val.thumbnail !== "" && val.title !='';
+      });
+      const hit =$('.hit li');       
+      for(let i =0;i<hit.length; i++){
+        hit.eq(i).css({'background-image':'url("'+origin[i].thumbnail+'")'});
+      let priceDot = origin[i].sale_price;
+      let dot = priceDot.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        hit.eq(i).append('<p>' + origin[i].title +'<br>'+dot+'원</p>');
       }
     });
 
-    //국내도서
-    $.ajax({
-      method: "GET",
-      url: "https://dapi.kakao.com/v3/search/book?target=contents",
-      data: {query: "소설", size:50},
-      headers: {Authorization: "KakaoAK 8244a385855c795ceaadd4a47e53aa38"}
-    })
-      .done(function( msg ) {
-        console.log(msg);
-        //오늘의 책 왼쪽
-        const today =$('.Lbook1');
-  
-        for(let i =0; i<today.length; i++){
-          $('.Lbook1').eq(i).prepend('<a href="#"><img src="'+msg.documents[i].thumbnail+'"/></a>');
-          $('.book1').eq(i).append('<h2>'+msg.documents[i].title+'</h2>');
-          $('.book1').eq(i).append('<p>'+msg.documents[i].authors+'</p>');
-          
-          let priceDot = msg.documents[i].sale_price;
-          let dot = priceDot.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-          $('.book1').eq(i).append('<span>10%</span>');
-          $('.book1').eq(i).append('<span>'+dot+'원</span>');
-          
-          let str = msg.documents[i].contents;
-          let str2 = str.substring(0,100);
-          $('.book1').eq(i).append('<p>'+str2+'</p>');
-        }
-  
-        //오늘의 책 오른쪽
-        const todaySlide=$('.slidebook1');
-        for(let i =0; i<todaySlide.length; i++){
-          $('.slidebook1').eq(i).append('<a href="#"><img src="'+msg.documents[i].thumbnail+'"/></a>');
-          $('.slidebook1').eq(i).append('<p>'+msg.documents[i].title+'</p>');
-        }
-      });
-
-      //eBook
-      $.ajax({
-        method: "GET",
-        url: "https://dapi.kakao.com/v3/search/book?target=contents",
-        data: {query: "에세이", size:50},
-        headers: {Authorization: "KakaoAK 8244a385855c795ceaadd4a47e53aa38"}
-      })
-        .done(function( msg ) {
-          console.log(msg);
-          //오늘의 책 왼쪽
-          const today =$('.Lbook2');
-    
-          for(let i =0; i<today.length; i++){
-            $('.Lbook2').eq(i).prepend('<a href="#"><img src="'+msg.documents[i].thumbnail+'"/></a>');
-            $('.book2').eq(i).append('<h2>'+msg.documents[i].title+'</h2>');
-            $('.book2').eq(i).append('<p>'+msg.documents[i].authors+'</p>');
-            
-            let priceDot = msg.documents[i].sale_price;
-            let dot = priceDot.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-            $('.book2').eq(i).append('<span>10%</span>');
-            $('.book2').eq(i).append('<span>'+dot+'원</span>');
-            
-            let str = msg.documents[i].contents;
-            let str2 = str.substring(0,100);
-            $('.book2').eq(i).append('<p>'+str2+'</p>');
-          }
-    
-          //오늘의 책 오른쪽
-          const todaySlide=$('.slidebook2');
-          for(let i =0; i<todaySlide.length; i++){
-            $('.slidebook2').eq(i).append('<a href="#"><img src="'+msg.documents[i].thumbnail+'"/></a>');
-            $('.slidebook2').eq(i).append('<p>'+msg.documents[i].title+'</p>');
-          }
-        });
-
-        //sam
-        $.ajax({
-          method: "GET",
-          url: "https://dapi.kakao.com/v3/search/book?target=publisher",
-          data: {query: "민음사", size:50},
-          headers: {Authorization: "KakaoAK 8244a385855c795ceaadd4a47e53aa38"}
-        })
-          .done(function( msg ) {
-            console.log(msg);
-            //오늘의 책 왼쪽
-            const today =$('.Lbook3');
-      
-            for(let i =0; i<today.length; i++){
-              $('.Lbook3').eq(i).prepend('<a href="#"><img src="'+msg.documents[i].thumbnail+'"/></a>');
-              $('.book3').eq(i).append('<h2>'+msg.documents[i].title+'</h2>');
-              $('.book3').eq(i).append('<p>'+msg.documents[i].authors+'</p>');
-              
-              let priceDot = msg.documents[i].sale_price;
-              let dot = priceDot.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-              $('.book3').eq(i).append('<span>10%</span>');
-              $('.book3').eq(i).append('<span>'+dot+'원</span>');
-              
-              let str = msg.documents[i].contents;
-              let str2 = str.substring(0,100);
-              $('.book3').eq(i).append('<p>'+str2+'</p>');
-            }
-      
-            //오늘의 책 오른쪽
-            const todaySlide=$('.slidebook3');
-            for(let i =0; i<todaySlide.length; i++){
-              $('.slidebook3').eq(i).append('<a href="#"><img src="'+msg.documents[i].thumbnail+'"/></a>');
-              $('.slidebook3').eq(i).append('<p>'+msg.documents[i].title+'</p>');
-            }
-          });
-
-          //핫트랙스
-          $.ajax({
-            method: "GET",
-            url: "https://dapi.kakao.com/v3/search/book?target=publisher",
-            data: {query: "창비", size:50},
-            headers: {Authorization: "KakaoAK 8244a385855c795ceaadd4a47e53aa38"}
-          })
-            .done(function( msg ) {
-              console.log(msg);
-              //오늘의 책 왼쪽
-              const today =$('.Lbook4');
-        
-              for(let i =0; i<today.length; i++){
-                $('.Lbook4').eq(i).prepend('<a href="#"><img src="'+msg.documents[i].thumbnail+'"/></a>');
-                $('.book4').eq(i).append('<h2>'+msg.documents[i].title+'</h2>');
-                $('.book4').eq(i).append('<p>'+msg.documents[i].authors+'</p>');
-                
-                let priceDot = msg.documents[i].sale_price;
-                let dot = priceDot.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-                $('.book4').eq(i).append('<span>10%</span>');
-                $('.book4').eq(i).append('<span>'+dot+'원</span>');
-                
-                let str = msg.documents[i].contents;
-                let str2 = str.substring(0,100);
-                $('.book4').eq(i).append('<p>'+str2+'</p>');
-              }
-        
-              //오늘의 책 오른쪽
-              const todaySlide=$('.slidebook4');
-              for(let i =0; i<todaySlide.length; i++){
-                $('.slidebook4').eq(i).append('<a href="#"><img src="'+msg.documents[i].thumbnail+'"/></a>');
-                $('.slidebook4').eq(i).append('<p>'+msg.documents[i].title+'</p>');
-              }
-            });
-
-            //교보ONly
-            $.ajax({
-              method: "GET",
-              url: "https://dapi.kakao.com/v3/search/book?target=contents",
-              data: {query: "교보문고", size:50},
-              headers: {Authorization: "KakaoAK 8244a385855c795ceaadd4a47e53aa38"}
-            })
-              .done(function( msg ) {
-                console.log(msg);
-                //오늘의 책 왼쪽
-                const today =$('.Lbook5');
-          
-                for(let i =0; i<today.length; i++){
-                  $('.Lbook5').eq(i).prepend('<a href="#"><img src="'+msg.documents[i].thumbnail+'"/></a>');
-                  $('.book5').eq(i).append('<h2>'+msg.documents[i].title+'</h2>');
-                  $('.book5').eq(i).append('<p>'+msg.documents[i].authors+'</p>');
-                  
-                  let priceDot = msg.documents[i].sale_price;
-                  let dot = priceDot.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-                  $('.book5').eq(i).append('<span>10%</span>');
-                  $('.book5').eq(i).append('<span>'+dot+'원</span>');
-                  
-                  let str = msg.documents[i].contents;
-                  let str2 = str.substring(0,100);
-                  $('.book5').eq(i).append('<p>'+str2+'</p>');
-                }
-          
-                //오늘의 책 오른쪽
-                const todaySlide=$('.slidebook5');
-                for(let i =0; i<todaySlide.length; i++){
-                  $('.slidebook5').eq(i).append('<a href="#"><img src="'+msg.documents[i].thumbnail+'"/></a>');
-                  $('.slidebook5').eq(i).append('<p>'+msg.documents[i].title+'</p>');
-                }
-              });
-           
